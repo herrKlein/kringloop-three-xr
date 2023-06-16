bd06c33413a47638c49b7625ade4af8e470c1b7a
herrKlein <github@kleinwerk.nl>
2023-06-16 10:07:20 +0200

    3 files modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React, { Suspense, useState } from 'react'
-import { Interactive, XR, ARButton, Controllers } from '@react-three/xr'
-import { Text } from '@react-three/drei'
-import './styles.css'
-import { Canvas } from '@react-three/fiber'
+import React, { Suspense, useState } from 'react';
+import { Interactive, XR, ARButton, Controllers } from '@react-three/xr';
+import { Text } from '@react-three/drei';
+import './index.css';
+import { Canvas } from '@react-three/fiber';
 
 function Box({ color, size, scale, children, ...rest }: any) {
   return (
@@ -11,31 +11,46 @@ function Box({ color, size, scale, children, ...rest }: any) {
       <meshPhongMaterial color={color} />
       {children}
     </mesh>
-  )
+  );
 }
 
 function Button(props: any) {
-  const [hover, setHover] = useState(false)
-  const [color, setColor] = useState<any>('blue')
+  const [hover, setHover] = useState(false);
+  const [color, setColor] = useState<any>('blue');
 
   const onSelect = () => {
-    setColor((Math.random() * 0xffffff) | 0)
-  }
+    setColor((Math.random() * 0xffffff) | 0);
+  };
 
   return (
-    <Interactive onHover={() => setHover(true)} onBlur={() => setHover(false)} onSelect={onSelect}>
-      <Box color={color} scale={hover ? [0.6, 0.6, 0.6] : [0.5, 0.5, 0.5]} size={[0.4, 0.1, 0.1]} {...props}>
+    <Interactive
+      onHover={() => setHover(true)}
+      onBlur={() => setHover(false)}
+      onSelect={onSelect}
+    >
+      <Box
+        color={color}
+        scale={hover ? [0.6, 0.6, 0.6] : [0.5, 0.5, 0.5]}
+        size={[0.4, 0.1, 0.1]}
+        {...props}
+      >
         <Suspense fallback={null}>
-          <Text position={[0, 0, 0.06]} fontSize={0.05} color="#000" anchorX="center" anchorY="middle">
+          <Text
+            position={[0, 0, 0.06]}
+            fontSize={0.05}
+            color="#000"
+            anchorX="center"
+            anchorY="middle"
+          >
             Hello react-xr!
           </Text>
         </Suspense>
       </Box>
     </Interactive>
-  )
+  );
 }
 
-export function App() {
+export default function App() {
   return (
     <>
       <ARButton />
@@ -48,5 +63,5 @@ export function App() {
         </XR>
       </Canvas>
     </>
-  )
+  );
 }

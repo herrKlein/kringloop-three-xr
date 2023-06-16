612bd9ce41398ccf8177454f9f86951554772b09
herrKlein <github@kleinwerk.nl>
2023-06-16 10:14:47 +0200

    1 file modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
 import React, { Suspense, useState } from 'react';
 import { Interactive, XR, ARButton, Controllers } from '@react-three/xr';
-import { Text } from '@react-three/drei';
+import { Text, Shadow } from '@react-three/drei';
 import './index.css';
 import { Canvas } from '@react-three/fiber';
 
-function Box({ color, size, scale, children, ...rest }: any) {
-  return (
-    <mesh scale={scale} {...rest}>
-      <boxBufferGeometry args={size} />
-      <meshPhongMaterial color={color} />
-      {children}
-    </mesh>
-  );
-}
-
-function Button(props: any) {
+function CylinderButton(props: any) {
   const [hover, setHover] = useState(false);
   const [color, setColor] = useState<any>('blue');
 
@@ -28,15 +18,14 @@ function Button(props: any) {
       onBlur={() => setHover(false)}
       onSelect={onSelect}
     >
-      <Box
-        color={color}
-        scale={hover ? [0.6, 0.6, 0.6] : [0.5, 0.5, 0.5]}
-        size={[0.4, 0.1, 0.1]}
-        {...props}
-      >
+      <group>
+        <mesh position={[0, 0.05, 0]} castShadow receiveShadow>
+          <cylinderBufferGeometry args={[0.1, 0.1, 0.1, 32]} />
+          <meshPhongMaterial color={color} />
+        </mesh>
         <Suspense fallback={null}>
           <Text
-            position={[0, 0, 0.06]}
+            position={[0, 0, 0.12]}
             fontSize={0.05}
             color="#000"
             anchorX="center"
@@ -45,7 +34,14 @@ function Button(props: any) {
             Hello react-xr!
           </Text>
         </Suspense>
-      </Box>
+        {hover && (
+          <Shadow
+            opacity={0.3}
+            scale={[0.6, 0.6, 0.6]}
+            rotation={[-Math.PI / 2, 0, 0]}
+          />
+        )}
+      </group>
     </Interactive>
   );
 }
@@ -54,11 +50,19 @@ export default function App() {
   return (
     <>
       <ARButton />
-      <Canvas>
+      <Canvas
+        shadows
+        dpr={[1, 2]}
+        camera={{ position: [0, 1.6, 3], near: 0.01, far: 100 }}
+      >
         <XR referenceSpace="local">
-          <ambientLight />
-          <pointLight position={[10, 10, 10]} />
-          <Button position={[0, 0.1, -0.2]} />
+          <ambientLight intensity={0.5} />
+          <pointLight position={[10, 10, 10]} intensity={1} castShadow />
+          <mesh rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
+            <planeBufferGeometry args={[10, 10]} />
+            <shadowMaterial opacity={0.3} />
+          </mesh>
+          <CylinderButton position={[0, 0.05, 0]} />
           <Controllers />
         </XR>
       </Canvas>
